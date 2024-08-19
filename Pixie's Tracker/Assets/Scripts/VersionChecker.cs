97a856bb8df40a9deec9aa1f608a33ac6abfe746
AudioPixie <90351518+AudioPixie@users.noreply.github.com>
2024-08-19 00:41:28 -0700

    Changed Version Number
    Also fixed notepad scroll sensitivity

--- Pixie's Tracker/Assets/Scripts/VersionChecker.cs
+++ Pixie's Tracker/Assets/Scripts/VersionChecker.cs
@@ -28,8 +28,8 @@ public class VersionChecker : MonoBehaviour
 
     public string remoteVersionUrl;
     public string localVersion;
-    public string docsURL = "https://github.com/AudioPixie/TPRTracker?tab=readme-ov-file#tprtracker";
-    public string latestReleaseURL = "https://github.com/AudioPixie/TPRTracker/releases";
+    public string docsURL;
+    public string latestReleaseURL;
 
     public Button UpdateResult;
 
@@ -50,9 +50,13 @@ public class VersionChecker : MonoBehaviour
     void Start()
     {
         remoteVersionUrl = "https://raw.githubusercontent.com/AudioPixie/TPRTracker/main/version.txt";
+        docsURL = "https://github.com/AudioPixie/TPRTracker?tab=readme-ov-file#tprtracker";
+        latestReleaseURL = "https://github.com/AudioPixie/TPRTracker/releases";
+
         localVersion = Application.version;
+
         UpdateResult.interactable = false;
-        Debug.LogWarning(remoteVersionUrl);
+        
         CheckForUpdate();
     }
 
@@ -60,30 +64,25 @@ public class VersionChecker : MonoBehaviour
     {
         UpdateResult.interactable = false;
         UpdateResult.GetComponentInChildren<TMP_Text>().text = "<color=#C900CA>Loading...</color>";
-        Debug.LogWarning(remoteVersionUrl);
+
         StartCoroutine(CheckVersionCoroutine(remoteVersionUrl));
     }
 
     IEnumerator CheckVersionCoroutine(string url)
     {
-        Debug.LogWarning(url);
         // Fetch remote version
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
-            Debug.LogWarning(url);
             yield return webRequest.SendWebRequest();
 
             if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
             {
                 UpdateResult.interactable = false;
                 UpdateResult.GetComponentInChildren<TMP_Text>().text = "<color=#FF4400>Connection failed, please try again.</color>";
-                Debug.LogWarning(webRequest.result);
             }
             else
             {
-                Debug.LogWarning(webRequest.result);
                 string remoteVersion = webRequest.downloadHandler.text.Trim();
-                Debug.LogWarning(remoteVersion);
                 CompareVersions(localVersion, remoteVersion);
             }
         }
